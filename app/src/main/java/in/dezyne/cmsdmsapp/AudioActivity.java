412afa616e6c1109c98738964886d716b71d6f72
dezynetech <dheeraj@dezyne.in>
2016-08-27 17:00:11 +0530

    added music

--- app/src/main/java/in/dezyne/cmsdmsapp/AudioActivity.java
+++ app/src/main/java/in/dezyne/cmsdmsapp/AudioActivity.java
@@ -1,17 +1,30 @@
 package in.dezyne.cmsdmsapp;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.media.AudioManager;
+import android.media.MediaMetadataRetriever;
+import android.media.MediaPlayer;
+import android.media.audiofx.Visualizer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.MediaController;
 import android.widget.Toast;
 import android.widget.VideoView;
 
+import java.io.IOException;
 
-/**
- * Created by Dezyne 2 on 8/25/2016.
- */
+
 public class AudioActivity extends Activity {
+
+    ImageView coverart;
+    MediaMetadataRetriever metaRetriver;
+    byte[] art;
+    String link;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -19,20 +32,45 @@ public class AudioActivity extends Activity {
 
         // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
+
+        coverart =(ImageView)findViewById(R.id.cover);
+
+
+       // metaRetriver = new MediaMetadataRetriever();
+       // metaRetriver.setDataSource("http://f6.wload.vc/files/sfd396/197677/Sab%20Tera_64(wapking.fm).mp3");
+
         try
         {
             setContentView(R.layout.audio);
-            String link="http://gaana99.com/fileDownload/Songs/128/28687.mp3";
-            VideoView videoView = (VideoView) findViewById(R.id.videoView2);
-            MediaController mediaController = new MediaController(this);
+             link="http://f6.wload.vc/files/sfd396/197677/Sab%20Tera_64(wapking.fm).mp3";
+            final VideoView videoView = (VideoView) findViewById(R.id.videoView2);
+            final MediaController mediaController = new MediaController(this);
             mediaController.setAnchorView(videoView);
-            Uri video = Uri.parse(link);
+            final Uri video = Uri.parse(link);
             videoView.setMediaController(mediaController);
             videoView.setVideoURI(video);
             videoView.start();
+            videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mediaPlayer) {
+                    mediaPlayer.reset();
+                    videoView.setVideoURI(video);
+                    videoView.start();
+                }
+            });
+
+
+
+            /*art = metaRetriver.getEmbeddedPicture();
+            Bitmap bm = BitmapFactory .decodeByteArray(art, 0, art.length);
+            coverart.setImageBitmap(bm);*/
+
+
         } catch (Exception e) {
             // TODO: handle exception
             Toast.makeText(this, "Error connecting", Toast.LENGTH_SHORT).show();
         }
     }
+
+
 }
